bd0fe58181c4e1a9b03c2b1fd1826ead52560b5f
Enzo <enzovillacorta.ev@gmail.com>
2023-09-14 21:17:48 +0800

    Debugged simple calculator

--- 2. Simple_Projects/simple_calculator.php
+++ 2. Simple_Projects/simple_calculator.php
@@ -1,40 +1,42 @@
 <?php
-// Define variables
-$firstNumber = $_POST['first number'];
-$secondNumber = $_POST['second number'];
-$operator = $_POST['operator'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Define variables
+    $firstNumber = $_POST['first_number'];
+    $secondNumber = $_POST['second_number'];
+    $operator = $_POST['operator'];
 
-// Data validation
-if (!is_numeric($firstNumber) || !is_numeric($secondNumber)) {
-    echo "Please enter valid numbers.";
-    return;
-}
-
-if (!in_array($operator, ['+', '-', '*', '/'])) {
-    echo "Invaid operator.";
-    return;
-}
-
-// Perform calculation
-function calculate($firstNumber, $secondNumber, $operator) {
-    switch ($operator) {
-        case '+':
-            return $firstNumber + $secondNumber;
-        case '-':
-            return $firstNumber - $secondNumber;
-        case '*':
-            return $firstNumber * $secondNumber;
-        case '/':
-                if ($secondNumber == 0) {
-                    return "Cannot divide by zero";
-                }
-                return $firstNumber / $secondNumber;
+    // Data validation
+    if (!is_numeric($firstNumber) || !is_numeric($secondNumber)) {
+        echo "Please enter valid numbers.";
+        return;
     }
-}
 
-// Display result
-$result = calculate($firstNumber, $secondNumber, $operator);
-echo "The result is: " . $result;
+    if (!in_array($operator, ['+', '-', '*', '/'])) {
+        echo "Invaid operator.";
+        return;
+    }
+
+    // Perform calculation
+    function calculate($firstNumber, $secondNumber, $operator) {
+        switch ($operator) {
+            case '+':
+                return $firstNumber + $secondNumber;
+            case '-':
+                return $firstNumber - $secondNumber;
+            case '*':
+                return $firstNumber * $secondNumber;
+            case '/':
+                    if ($secondNumber == 0) {
+                        return "Cannot divide by zero";
+                    }
+                    return $firstNumber / $secondNumber;
+        }
+    }
+
+    // Display result
+    $result = calculate($firstNumber, $secondNumber, $operator);
+    echo "The result is: " . $result;
+}
 ?>
 
 <!DOCTYPE html>
@@ -48,14 +50,14 @@ echo "The result is: " . $result;
 
 <h1>Simple Calculator</h1>
 
-<form action="calculator.php" method="post">
+<form action="simple_calculator.php" method="post">
     <!-- Input for first number-->
-    <label for="first number">Number 1:</label>
-    <input type="number" id="first number" name="first number">
+    <label for="first_number">Number 1:</label>
+    <input type="number" id="first_number" name="first_number">
     <br>
     <!-- Input for second number-->
-    <label for="second number">Number 2:</label>
-    <input type="number" id="second number" name="second number">
+    <label for="second_number">Number 2:</label>
+    <input type="number" id="second_number" name="second_number">
     <br>
     <!-- Dropdown for operator-->
     <label for="operator">Operator:</label>
