3338d98de65a40868bc9dda27b7c9df546138a39
Enzo <enzovillacorta.ev@gmail.com>
2023-09-14 23:54:23 +0800

    Building To-do list

---
+++ 2. Simple_Projects/to-do-list.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TO-DO LIST</title>
+</head>
+<body>
+
+    <h1>To-do List</h1>
+
+    <h2>This is a simple yet intuitive to-do list</h2>
+
+    <!-- Input field for new task -->
+    <input type="text" id="newTask" placeholder="Enter new task">
+
+    <!-- Button to add new task -->
+    <button id="addTaskButton">Add</button>
+
+    <!-- Area to display the list of tasks -->
+    <ul id="taskList">
+        <!-- Tasks will be added here -->
+    </ul>
+
+</body>
+</html>
