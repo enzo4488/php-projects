d83d44f2b5c2350dd3c59bffd0e2a8322c8c9cd5
Enzo <enzovillacorta.ev@gmail.com>
2023-09-14 21:07:21 +0800

    Added simple projects directory

--- 1.TicTacToe/leaderboard.php
+++ 1.TicTacToe/leaderboard.php
@@ -1,3 +1,7 @@
 <?php
 
 include_once 'includes/database.php';
+$userStats = fetchUserStats($username);
+echo "Wins: " . $userStats['wins'];
+echo "Losses: " . $userStats['losses'];
+echo "Draws: " . $userStats['draws'];
---
+++ 2. Simple_Projects/simple_calculator.php
@@ -0,0 +1,75 @@
+<?php
+// Define variables
+$firstNumber = $_POST['first number'];
+$secondNumber = $_POST['second number'];
+$operator = $_POST['operator'];
+
+// Data validation
+if (!is_numeric($firstNumber) || !is_numeric($secondNumber)) {
+    echo "Please enter valid numbers.";
+    return;
+}
+
+if (!in_array($operator, ['+', '-', '*', '/'])) {
+    echo "Invaid operator.";
+    return;
+}
+
+// Perform calculation
+function calculate($firstNumber, $secondNumber, $operator) {
+    switch ($operator) {
+        case '+':
+            return $firstNumber + $secondNumber;
+        case '-':
+            return $firstNumber - $secondNumber;
+        case '*':
+            return $firstNumber * $secondNumber;
+        case '/':
+                if ($secondNumber == 0) {
+                    return "Cannot divide by zero";
+                }
+                return $firstNumber / $secondNumber;
+    }
+}
+
+// Display result
+$result = calculate($firstNumber, $secondNumber, $operator);
+echo "The result is: " . $result;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Calculator in PHP</title>
+</head>
+<body>
+
+<h1>Simple Calculator</h1>
+
+<form action="calculator.php" method="post">
+    <!-- Input for first number-->
+    <label for="first number">Number 1:</label>
+    <input type="number" id="first number" name="first number">
+    <br>
+    <!-- Input for second number-->
+    <label for="second number">Number 2:</label>
+    <input type="number" id="second number" name="second number">
+    <br>
+    <!-- Dropdown for operator-->
+    <label for="operator">Operator:</label>
+    <select id="operator" name="operator"> 
+        <option value="+">Addition (+)</option>
+        <option value="-">Subtraction (-)</option>
+        <option value="*">Multiplication (*)</option>
+        <option value="/">Division (/)</option>
+    </select>
+    <br>
+
+    <!-- Submit button -->
+    <input type="submit" value="Calculate">
+</form>
+
+</body>
+</html>
