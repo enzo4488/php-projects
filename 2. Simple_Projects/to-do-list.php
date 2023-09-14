c2d6164ada8e296506ab5b3fc74faa2c9ee19616
Enzo <enzovillacorta.ev@gmail.com>
2023-09-15 01:50:26 +0800

    to-do list cont.

--- 2. Simple_Projects/to-do-list.php
+++ 2. Simple_Projects/to-do-list.php
@@ -1,4 +1,13 @@
 <?php
+$taskList = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $newTask = $_POST['newTask'];
+    
+    if (!empty($newTask)) {
+        $taskList[] = $newTask;
+    }
+}
 
 ?>
 
@@ -15,15 +24,20 @@
 
     <h2>This is a simple yet intuitive to-do list</h2>
 
+    <form action=" " method="post">
     <!-- Input field for new task -->
-    <input type="text" id="newTask" placeholder="Enter new task">
-
+    <input type="text" name="newTask" placeholder="Enter new task">
     <!-- Button to add new task -->
-    <button id="addTaskButton">Add</button>
-
+    <button type="submit">Add</button>
+    </form>
     <!-- Area to display the list of tasks -->
     <ul id="taskList">
         <!-- Tasks will be added here -->
+        <?php
+        foreach ($taskList as $task) {
+            echo "<li>" . htmlspecialchars($task) . "</li>";
+        }
+        ?>
     </ul>
 
 </body>
